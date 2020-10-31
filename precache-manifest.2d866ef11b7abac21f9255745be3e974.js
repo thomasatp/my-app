2d73d32030eec5d7dde5df999cee707301837aa6
Thomas Drapeau <drapeau.th@gmail.com>
2020-10-31 10:09:39 +0100

    Updates

---
+++ precache-manifest.2d866ef11b7abac21f9255745be3e974.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5defd668d7683d6c4269ada815f8dcd1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "fa45e2dc42f80f3dd1a8",
+    "url": "/my-app/static/css/main.14b585a1.chunk.css"
+  },
+  {
+    "revision": "589b6ef8f3db83dfb1c2",
+    "url": "/my-app/static/js/2.f51ed56d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.f51ed56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa45e2dc42f80f3dd1a8",
+    "url": "/my-app/static/js/main.89c32ce8.chunk.js"
+  },
+  {
+    "revision": "2a4828a1f8612c036a83",
+    "url": "/my-app/static/js/runtime-main.00fe1762.js"
+  },
+  {
+    "revision": "55612daf5677b73c2a03961de78dae1e",
+    "url": "/my-app/static/media/Helvetica73BoldExtended.55612daf.woff"
+  },
+  {
+    "revision": "a3397d1516673140a76f853fabf629c6",
+    "url": "/my-app/static/media/account.a3397d15.svg"
+  },
+  {
+    "revision": "2c6dad0ea19b8a9260d3df12ef359751",
+    "url": "/my-app/static/media/bag.2c6dad0e.svg"
+  },
+  {
+    "revision": "34d26c7e4d136a233d0e57546c63d081",
+    "url": "/my-app/static/media/burger.34d26c7e.svg"
+  },
+  {
+    "revision": "1452c037252cd328ad6623e78437da2a",
+    "url": "/my-app/static/media/logo1.1452c037.svg"
+  },
+  {
+    "revision": "c4b088fab6b81839042a91eb8f4d646e",
+    "url": "/my-app/static/media/logo2.c4b088fa.svg"
+  },
+  {
+    "revision": "a4d8def72f2eadd8c0e0deda7d018c2e",
+    "url": "/my-app/static/media/search.a4d8def7.svg"
+  },
+  {
+    "revision": "4fa696b0bebdb0fc3b7a1e0ba9724b4c",
+    "url": "/my-app/static/media/storelocator.4fa696b0.svg"
+  },
+  {
+    "revision": "6521032fce453e0beeba08355b8da15a",
+    "url": "/my-app/static/media/video.6521032f.mp4"
+  },
+  {
+    "revision": "cf3aa221c1849e07c7c1d42d0832d070",
+    "url": "/my-app/static/media/video2.cf3aa221.mp4"
+  },
+  {
+    "revision": "187e0161636de069aea41608cdd47e4f",
+    "url": "/my-app/static/media/video3.187e0161.mp4"
+  }
+]);
