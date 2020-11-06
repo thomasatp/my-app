9354718c517a03a5415b9321b6a1a7251cb9d4ac
Thomas Drapeau <drapeau.th@gmail.com>
2020-11-06 13:49:58 +0100

    Updates

---
+++ precache-manifest.2ba42706b16ffe9b8f5973bffd816a34.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd360eba9b9c0394ba9e57b35faa0fc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ad87f490273ff80322c2",
+    "url": "/my-app/static/css/main.b50c7fd4.chunk.css"
+  },
+  {
+    "revision": "88979c3bf9e3759be356",
+    "url": "/my-app/static/js/2.5a95c050.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.5a95c050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad87f490273ff80322c2",
+    "url": "/my-app/static/js/main.cf4871ca.chunk.js"
+  },
+  {
+    "revision": "2a4828a1f8612c036a83",
+    "url": "/my-app/static/js/runtime-main.00fe1762.js"
+  },
+  {
+    "revision": "55612daf5677b73c2a03961de78dae1e",
+    "url": "/my-app/static/media/Helvetica73BoldExtended.55612daf.woff"
+  },
+  {
+    "revision": "788c9e46711e92cdaf2137ea07e9f454",
+    "url": "/my-app/static/media/MenClothing1.788c9e46.jpg"
+  },
+  {
+    "revision": "822906fba4b845cf4d9d904f7f6df924",
+    "url": "/my-app/static/media/MenClothing2.822906fb.jpg"
+  },
+  {
+    "revision": "a3397d1516673140a76f853fabf629c6",
+    "url": "/my-app/static/media/account.a3397d15.svg"
+  },
+  {
+    "revision": "2c6dad0ea19b8a9260d3df12ef359751",
+    "url": "/my-app/static/media/bag.2c6dad0e.svg"
+  },
+  {
+    "revision": "4a04fb528d6d9f7faa590a399c0bd2ca",
+    "url": "/my-app/static/media/bag1.4a04fb52.jpg"
+  },
+  {
+    "revision": "d9f8537eb6e56c0f51c24b7a32c7ded0",
+    "url": "/my-app/static/media/bag2.d9f8537e.jpg"
+  },
+  {
+    "revision": "34d26c7e4d136a233d0e57546c63d081",
+    "url": "/my-app/static/media/burger.34d26c7e.svg"
+  },
+  {
+    "revision": "184b939565e468a8298c9873e01b1086",
+    "url": "/my-app/static/media/fragrances.184b9395.jpg"
+  },
+  {
+    "revision": "c0bbee4067ba61677fbf785ed519d14f",
+    "url": "/my-app/static/media/giftmen1.c0bbee40.jpg"
+  },
+  {
+    "revision": "59ec27562afd1bfa65308b588b8fcb0d",
+    "url": "/my-app/static/media/giftmen2.59ec2756.jpg"
+  },
+  {
+    "revision": "6a9a91717139a703a162fc0587849245",
+    "url": "/my-app/static/media/image2.6a9a9171.jpg"
+  },
+  {
+    "revision": "9f4b8b50eb5cb5d091e4a1993026c6e9",
+    "url": "/my-app/static/media/kids.9f4b8b50.jpg"
+  },
+  {
+    "revision": "b7aea22e0cda8bc5f7737406640b5baf",
+    "url": "/my-app/static/media/leather.b7aea22e.jpg"
+  },
+  {
+    "revision": "1452c037252cd328ad6623e78437da2a",
+    "url": "/my-app/static/media/logo1.1452c037.svg"
+  },
+  {
+    "revision": "c4b088fab6b81839042a91eb8f4d646e",
+    "url": "/my-app/static/media/logo2.c4b088fa.svg"
+  },
+  {
+    "revision": "9dd4d32dbeea3fd8e219abdc9540d1cc",
+    "url": "/my-app/static/media/men.9dd4d32d.jpg"
+  },
+  {
+    "revision": "a4d8def72f2eadd8c0e0deda7d018c2e",
+    "url": "/my-app/static/media/search.a4d8def7.svg"
+  },
+  {
+    "revision": "4fa696b0bebdb0fc3b7a1e0ba9724b4c",
+    "url": "/my-app/static/media/storelocator.4fa696b0.svg"
+  },
+  {
+    "revision": "6521032fce453e0beeba08355b8da15a",
+    "url": "/my-app/static/media/video.6521032f.mp4"
+  },
+  {
+    "revision": "cf3aa221c1849e07c7c1d42d0832d070",
+    "url": "/my-app/static/media/video2.cf3aa221.mp4"
+  },
+  {
+    "revision": "187e0161636de069aea41608cdd47e4f",
+    "url": "/my-app/static/media/video3.187e0161.mp4"
+  },
+  {
+    "revision": "b7f1c72fb902f2991ba4720cab60ec17",
+    "url": "/my-app/static/media/watch.b7f1c72f.jpg"
+  },
+  {
+    "revision": "37287268c668b64bf07c61616194f14c",
+    "url": "/my-app/static/media/women.37287268.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.c4305fe49bfac733a7f79da798f7956e.js"
+  "/my-app/precache-manifest.2ba42706b16ffe9b8f5973bffd816a34.js"
 );
 
 self.addEventListener('message', (event) => {
